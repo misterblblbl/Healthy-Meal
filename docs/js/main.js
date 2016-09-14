56f8b2d4f76e55b957668397364f73d87836b3ce
Alexandra Godun <mister.blblbl@gmail.com>
2016-09-14 16:36:31 +0300

    Добавила публикацию и сборку

---
+++ docs/js/main.js
@@ -0,0 +1,44 @@
+
+var menuToggle = document.querySelector('.main-nav__toggle');
+var menuWrapper = document.querySelector('.main-nav__wrapper');
+
+menuToggle.addEventListener('click', function(){
+    menuWrapper.classList.toggle('main-nav__wrapper--closed');
+    if (menuToggle.classList.contains('main-nav__toggle--closed')) {
+        menuToggle.classList.remove('main-nav__toggle--closed');
+        menuToggle.classList.add('main-nav__toggle--opened');
+        menuWrapper.classList.add('main-nav__wrapper--opened');
+    } else if (menuToggle.classList.contains('main-nav__toggle--opened')) {
+        menuToggle.classList.remove('main-nav__toggle--opened');
+        menuToggle.classList.add('main-nav__toggle--closed');
+        menuWrapper.classList.remove('main-nav__wrapper--opened');
+        menuWrapper.classList.add('main-nav__wrapper--closed');
+    }
+});
+
+$(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+
+    $('.tastes-slider__items').slick({
+        infinite: true,
+        speed: 300,
+        swipeToSlide: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: true,
+        prevArrow: $('.tastes-slider__controls--left'),
+        nextArrow: $('.tastes-slider__controls--right')
+    });
+});
+
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,17 @@ var less = require("gulp-less");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var mqpacker = require("css-mqpacker");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
 var server = require("browser-sync");
-var webp = require("gulp-webp");
+var clean = require("gulp-clean");
+var runSequence = require('gulp-run-sequence');
 
+//Собрать стилевой файл, добавить префиксы,
+// скомбинировать медиа-вырадения, минифицировать файл
 gulp.task("style", function() {
-    gulp.src("less/style.less")
+    return gulp.src("less/style.less")
         .pipe(plumber())
         .pipe(less())
         .pipe(postcss([
@@ -20,28 +26,77 @@ gulp.task("style", function() {
                 "last 2 Opera versions",
                 "last 2 Edge versions"
             ]})
+            /*,
+             mqpacker({
+             sort: true
+             })
+             */
         ]))
-        .pipe(gulp.dest("css"))
-        .pipe(server.reload({stream: true}));
+        .pipe(gulp.dest("build/css"))
+        .pipe(minify())
+        .pipe(rename("style.min.css"))
+        .pipe(gulp.dest("build/css"));
 });
 
-gulp.task("serve", ["style"], function() {
+//Скопировать изображения
+gulp.task("copy:images", function() {
+    return gulp.src("img/*.{png,jpg,svg,webp}")
+        .pipe(gulp.dest("build/img"));
+});
+
+//Минифицировать и скопировать js-файлы
+gulp.task("js", function() {
+    return gulp.src("js/*.js")
+        .pipe(plumber())
+        .pipe(gulp.dest("build/js"))
+        .pipe(rename({
+            suffix:".min",
+            extname: "js"
+        }))
+        .pipe(gulp.dest("build/js"));
+});
+
+//Скопировать щрифты
+gulp.task("copy:fonts", function() {
+    return gulp.src("fonts/**/*.{woff,woff2,eot}")
+        .pipe(gulp.dest("build/fonts"))
+});
+
+//Скопировать html-файлы
+gulp.task("copy:html", function() {
+    return gulp.src("*.html")
+        .pipe(gulp.dest("build"));
+});
+
+//Очистить папку build при повторной сборке
+gulp.task("clean", function () {
+    return gulp.src("build", {read: false})
+        .pipe(clean({force: true}))
+});
+
+//Собрать проект
+gulp.task("build", function() {
+    runSequence(
+        "clean",
+        "copy:fonts",
+        "copy:html",
+        "copy:images",
+        "style",
+        "js"
+    );
+});
+
+gulp.task("serve", function() {
     server.init({
-        server: ".",
+        server: "build",
         notify: false,
         open: true,
         ui: false
     });
 
-    gulp.watch("less/**/*.less", ["style"]);
-    gulp.watch("*.html").on("change", server.reload);
+    gulp.watch("js/*.js", ["js"]);
+    gulp.watch("less/*.less", ["style"]);
+    gulp.watch("*.html", ["copy:html"]);
 });
 
-gulp.task("webp", function () {
-    return gulp.src("img/*.png")
-        .pipe(webp({
-            quality: 94,
-            method: 6
-        }))
-        .pipe(gulp.dest("img"));
-});
+gulp.task('default', ['build']);
